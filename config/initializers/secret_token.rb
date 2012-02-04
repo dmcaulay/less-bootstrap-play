6319eaa1a53736e1901d3ccde825039a3695f495
Dan McAulay <dmcaulay@gmail.com>
2012-02-04 15:01:28 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessBootstrapPlay::Application.config.secret_token = '2239ba5395b100e2c9967ffb9be128c81c0365ef3c62c0617ec0543e028709c9741c1f50fb0d1ae94c877faebf18936f7cf225f7b8065f24876426b241b46006'
